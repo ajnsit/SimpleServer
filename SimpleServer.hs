ae1b1756a27a0350eb9b8d99c12ed7d65a7561bc
Anupam Jain <ajnsit@gmail.com>
2012-09-02 16:49:42 +0530

    Added port configuration option

--- SimpleServer.hs
+++ SimpleServer.hs
@@ -1,9 +1,23 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
 module Main where
 
 import Network.Wai.Application.Static
 import Network.Wai.Handler.Warp
+import System.Console.CmdArgs
 
 main :: IO ()
-main = run 8080 $ staticApp $ defaultFileServerSettings "."
+main = do
+  simpleServer <- cmdArgs $ SimpleServer
+    { port = 8000 &= help "Port on which the server runs" &= opt (8000::Int) }
+    &= summary "SimpleServer v0.1"
+  let p = port simpleServer
+  putStrLn $ "Running simpleserver on port " ++ (show p)
+  run p $ staticApp $ defaultFileServerSettings "."
+
+
+-- Command line arguments
+data SimpleServer = SimpleServer
+  { port :: Int
+  }
+  deriving (Data, Typeable)
 
