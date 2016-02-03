1e4b1dfce2ca4c0f7199784d978fec15b00d8b6f
Anupam Jain <ajnsit@gmail.com>
2016-02-03 12:28:04 +0530

    Add Dyre

--- src/SimpleServer.hs
+++ src/SimpleServer.hs
@@ -3,34 +3,48 @@ module Main where
 
 import Control.Monad (when)
 
+import qualified Config.Dyre as Dyre
 import Network.Wai.Middleware.Routes
 import Network.Wai.Handler.Warp
 import System.Console.CmdArgs
 
 main :: IO ()
 main = do
-  settings <- cmdArgs $
-    simpleServerCmdArgs
-    &= summary "SimpleServer v0.1.1"
+  settings <- fetchCmdArgs
   let p = port settings
   putStrLn $ "SimpleServer running on port " ++ show p
   run p $ waiApp $ application settings
 
-application :: SimpleServer -> RouteM ()
+
+---------------------
+-- WAI Application --
+---------------------
+
+application :: SimpleServerConfig -> RouteM ()
 application settings = do
   when (loglevel settings > 0) $ middleware logStdoutDev
   catchall $ staticApp $ defaultFileServerSettings $ static settings
 
--- Command line arguments
-data SimpleServer = SimpleServer
+
+----------------------------
+-- Command line arguments --
+----------------------------
+
+data SimpleServerConfig = SimpleServerConfig
   { port :: Int
   , static :: String
   , loglevel :: Int
   }
   deriving (Data, Typeable)
 
-simpleServerCmdArgs :: SimpleServer
-simpleServerCmdArgs = SimpleServer
+fetchCmdArgs :: IO SimpleServerConfig
+fetchCmdArgs = cmdArgs $
+    simpleServerCmdArgs
+    &= program "simpleserver"
+    &= summary "SimpleServer v0.1.1"
+
+simpleServerCmdArgs :: SimpleServerConfig
+simpleServerCmdArgs = SimpleServerConfig
     { port = 8000
         &= help "Port on which the server runs (default 8000)"
         &= opt (8000::Int)
@@ -40,4 +54,22 @@ simpleServerCmdArgs = SimpleServer
     , loglevel = 0
         &= help "Logging level (default 0)"
         &= opt (0::Int)
+    }
+
+
+-----------------
+-- Dyre Config --
+-----------------
+
+confError :: String -> String -> String
+confError cfgMessage error = "Error:" ++ error ++ "\n" ++ cfgMessage
+
+realMain message = do
+    putStrLn "Entered Main Function"
+    putStrLn message
+
+dyreExample = Dyre.wrapMain Dyre.defaultParams
+    { Dyre.projectName  = "dyreExample"
+    , Dyre.showError    = confError
+    , Dyre.realMain     = realMain
     }
